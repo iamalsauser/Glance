231222ff059e90fd85816594b7789648599c0c61
Iamalsauser <parthsinhv4@gmail.com>
2024-10-07 23:56:02 +0530

    update:Auth

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:material_color_utilities/material_color_utilities.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
